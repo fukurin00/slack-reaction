95c5dda28164920843dfa2ce67dbfa321a648b3a
yusuke <maroemon58@gmail.com>
2018-08-06 15:53:37 +0900

    Add non reaction pattern Fix code

--- main.js
+++ main.js
@@ -54,6 +54,42 @@ function getReactions(responses) {
   });
 }
 
+sortReaction = reactions => {
+  // counter
+  totalReaction = [];
+
+  reactions.map(reaction => {
+    name = ':' + reaction.name + ':';
+    if (name in totalReaction === false) {
+      totalReaction[name] = 0;
+    }
+    totalReaction[name] += reaction.count;
+  });
+
+  // convert for sort
+  sortReaction = [];
+  for (name in totalReaction) {
+    sortReaction.push({
+      name: name,
+      count: totalReaction[name]
+    });
+  }
+
+  sortReaction = sortReaction.sort((a, b) => {
+    if (a.count > b.count) return -1;
+    else if (a.count < b.count) return 1;
+    else return 0;
+  });
+
+  // create send message
+  msg = '';
+  for (reaction of sortReaction) {
+    msg += reaction.name + ' : ' + reaction.count + '\n';
+  }
+
+  return msg;
+};
+
 // oldest ※前日0時0分
 oldest = moment()
   .subtract(1, 'days')
@@ -64,41 +100,19 @@ latest = moment().startOf('day');
 getChannels(oldest.unix(), latest.unix())
   .then(getReactions)
   .then(reactions => {
-    // counter
-    totalReaction = [];
-
-    reactions.map(reaction => {
-      name = ':' + reaction.name + ':';
-      if (name in totalReaction === false) {
-        totalReaction[name] = 0;
-      }
-      totalReaction[name] += reaction.count;
-    });
-
-    // convert for sort
-    sortReaction = [];
-    for (name in totalReaction) {
-      sortReaction.push({
-        name: name,
-        count: totalReaction[name]
-      });
+    if (reactions.length === 0) {
+      msg =
+        'Good Morning!! ' +
+        oldest.format('M/D(ddd)') +
+        'のリアクションは・・・なしですyo〜';
+    } else {
+      msg =
+        'Good Morning!! ' +
+        oldest.format('M/D(ddd)') +
+        'のリアクション集計したyo〜\n\n';
+      msg += sortReaction(reactions);
     }
 
-    sortReaction = sortReaction.sort((a, b) => {
-      if (a.count > b.count) return -1;
-      else if (a.count < b.count) return 1;
-      else return 0;
-    });
-    console.log(sortReaction);
-
-    // create send message
-    msg =
-      'Good Morning!! ' +
-      oldest.format('M/D(ddd)') +
-      'のリアクション集計したyo〜\n\n';
-    for (reaction of sortReaction) {
-      msg += reaction.name + ' : ' + reaction.count + '\n';
-    }
     console.log(msg);
 
     slack.chat.postMessage({
