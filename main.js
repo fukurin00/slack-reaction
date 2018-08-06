5980b7bce168993ee0f409798467ef6c8d2dd411
yusuke <maroemon58@gmail.com>
2018-08-06 12:02:52 +0900

    Fix how to use SLACK API

--- main.js
+++ main.js
@@ -1,53 +1,63 @@
 // load .env
 require('dotenv').config();
-
+const util = require('util');
 const slack = require('slack');
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 
-// oldest
-ts = Math.round(new Date('2018/08/03').getTime() / 1000);
+function getChannels(ts) {
+  return new Promise(function(onFulfilled, onRejected) {
+    slack.channels.list({ token: SLACK_TOKEN }).then(response => {
+      onFulfilled(response.channels.map(channel => channel.id), ts);
+    });
+  });
+}
 
-// get channel list
-slack.channels
-  .list({ token: SLACK_TOKEN })
-  .then(response => {
-    response.channels.map(channel => {
-      console.log(channel.id + ' : ' + channel.name);
-      // get conversations
-      slack.conversations
+function getReactions(channels, ts) {
+  return new Promise(function(onFulfilled, onRejected) {
+    reactions = [];
+    let i = 0;
+    channels.map(channel => {
+      slack.channels
         .history({
           token: SLACK_TOKEN,
-          channel: channel.id,
+          channel: channel,
           oldest: ts
         })
         .then(response => {
           response.messages.map(message => {
-            // get reactions
-            slack.reactions
-              .get({
-                token: SLACK_TOKEN,
-                channel: channel.id,
-                timestamp: message.ts
-              })
-              .then(response => {
-                if (response.message.hasOwnProperty('reactions')) {
-                  console.log(response.message.reactions);
-                }
-              })
-              .catch(error => {
-                console.log('get reaction error!!');
-                console.error(error);
-              });
+            if ('reactions' in message) {
+              reactions = reactions.concat(message.reactions);
+            }
           });
+          if (++i == channels.length) {
+            onFulfilled(reactions);
+          }
         })
         .catch(error => {
-          console.log('conversations error!!');
-          console.error(error);
+          console.log('error!!');
         });
     });
-  })
-  .catch(error => {
-    console.log('channel list error!!');
-    console.error(error);
+  });
+}
+
+// oldest
+ts = Math.round(new Date('2018/08/03').getTime() / 1000);
+
+getChannels(ts)
+  .then(getReactions)
+  .then(reactions => {
+    // counter
+    totalReaction = [];
+
+    reactions.map(reaction => {
+      name = ':' + reaction.name + ':';
+      if (name in totalReaction === false) {
+        totalReaction[name] = 0;
+      }
+      totalReaction[name] += reaction.count;
+    });
+
+    // result
+    console.log(util.inspect(totalReaction, { maxArrayLength: null }));
   });
